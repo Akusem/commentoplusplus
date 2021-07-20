5914603fb4f2627c7876846d99b6982df2566a54
Akusem <nathanael.z@outlook.fr>
2021-07-20 18:04:08 +0200

    dashboard-general.js: add auto query of all labels

--- frontend/js/dashboard-general.js
+++ frontend/js/dashboard-general.js
@@ -7,6 +7,8 @@
   global.generalOpen = function() {
     $(".view").hide();
     $("#general-view").show();
+
+    global.setAllLabels();
   };
 
   global.generalSaveHandler = function() {
@@ -48,6 +50,26 @@
 
   global.closeLabelCreator = function() {
     $("#label-creator").hide()
+  }
+
+  global.setAllLabels = function() {
+    // Get the list of labels already created if labels are allowed
+    var data = global.dashboard.$data;
+    
+    if (data.domains[data.cd].allowLabels) {
+      var json = {
+        "ownerToken": global.cookieGet("commentoOwnerToken"),
+        "domain": data.domains[data.cd].domain
+      };
+      global.post(global.origin + "/api/label/owner/listAll", json, function(resp) {
+        if (!resp.success) {
+          global.globalErrorShow(resp.message);
+          return
+        }
+        
+        Vue.set(data.domains[data.cd], "labelsAll", resp.labels)
+      });
+    }
   }
 
 } (window.commento, document));
