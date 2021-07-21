d92687d0dac1bbc2c981a9becf98349e7e29a990
Akusem <nathanael.z@outlook.fr>
2021-07-21 12:03:24 +0200

    sql: add label's name and relationship with comment

--- db/20210719152525-add-label.sql
+++ db/20210719152525-add-label.sql
@@ -1,11 +1,17 @@
 -- Add label table and foreign key in comments
-
 CREATE TABLE IF NOT EXISTS labels (
   labelHex               TEXT          NOT NULL  UNIQUE  PRIMARY KEY      ,
+  name                   TEXT          NOT NULL  UNIQUE                   ,
   color                  TEXT          NOT NULL                           ,
   domain                 TEXT          NOT NULL
 );
 
-ALTER TABLE comments ADD COLUMN IF NOT EXISTS labelHex TEXT;
+-- Many to many relationship through comments and labels
+CREATE TABLE IF NOT EXISTS comments_labels (
+  commentHex             TEXT         REFERENCES comments (commentHex) ON UPDATE CASCADE,
+  labelHex               TEXT         REFERENCES labels (labelHex)    ON UPDATE CASCADE,
+  CONSTRAINT comments_labels_pkey     PRIMARY KEY (commentHex, labelHex)
+);
 
+-- Create a field to make labels optionable
 ALTER TABLE domains ADD COLUMN IF NOT EXISTS allowLabels BOOLEAN DEFAULT FALSE;
