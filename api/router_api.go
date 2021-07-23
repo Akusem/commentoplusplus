806de129672f6958f9a808e340e0f1b3b9b2ee46
Akusem <nathanael.z@outlook.fr>
2021-07-23 14:22:27 +0200

    router_api.go: create route for commentLabelNew

--- api/router_api.go
+++ api/router_api.go
@@ -66,6 +66,7 @@ func apiRouterInit(router *mux.Router) error {
 	router.HandleFunc("/api/comment/vote", commentVoteHandler).Methods("POST")
 	router.HandleFunc("/api/comment/approve", commentApproveHandler).Methods("POST")
 	router.HandleFunc("/api/comment/delete", commentDeleteHandler).Methods("POST")
+	router.HandleFunc("/api/comment/label/new", commentLabelNewHandler).Methods("POST")
 	router.HandleFunc("/api/comment/owner/list", commentListApprovalsHandler).Methods("POST")
 	router.HandleFunc("/api/comment/owner/listAll", commentListAllHandler).Methods("POST")
 	router.HandleFunc("/api/comment/owner/approve", commentOwnerApproveHandler).Methods("POST")
