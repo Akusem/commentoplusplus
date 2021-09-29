cfcf936b0f0a63b154bff0157ff525709a758933
Akusem <nathanael.z@outlook.fr>
2021-09-29 18:34:20 +0200

    dashboard-general.js: add logic to edit label

--- frontend/js/dashboard-general.js
+++ frontend/js/dashboard-general.js
@@ -46,10 +46,27 @@
 
   global.openLabelCreator = function() {
     $("#label-creator").show()
+    // Avoid having label Creator & Editor open at the same time
+    global.closeLabelEditor()
   }
 
   global.closeLabelCreator = function() {
     $("#label-creator").hide()
+  }
+
+  global.openLabelEditor = function(label) {
+    var data = global.dashboard.$data
+    // Set label Editor to label of interest value
+    data.domains[data.cd].editLabelHex = label.labelHex;
+    data.domains[data.cd].editLabelName = label.name;
+    data.domains[data.cd].editLabelColor = label.color;
+    $("#label-editor").show()
+    // Avoid having label Creator & Editor open at the same time
+    global.closeLabelCreator()
+  }
+
+  global.closeLabelEditor = function() {
+    $("#label-editor").hide()
   }
 
   global.setAllLabels = function() {
@@ -92,6 +109,27 @@
       }
 
       global.closeLabelCreator();
+      global.setAllLabels();
+    });
+  }
+
+  global.editLabel = function() {
+    var data = global.dashboard.$data
+    var json = {
+      "ownerToken": global.cookieGet("commentoOwnerToken"),
+      "domain": data.domains[data.cd].domain,
+      "name": data.domains[data.cd].editLabelName,
+      "color": data.domains[data.cd].editLabelColor,
+      "labelHex": data.domains[data.cd].editLabelHex,
+    };
+
+    global.post(global.origin + "/api/label/edit", json, function(resp) {
+      if (!resp.success) {
+        global.globalErrorShow(resp.message);
+        return
+      }
+      // Reload all labels, new one included
+      global.closeLabelEditor();
       global.setAllLabels();
     });
   }
